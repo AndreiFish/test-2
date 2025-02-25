34a3778b323a8fcf2ca791e6b9b0d299ffe9666b
AndreiFish <and.zhandrov@gmail.com>
2025-02-26 02:40:36 +0400

    fix bug

---
+++ homework 3.js
@@ -0,0 +1,13 @@
+const titleProject = 'Оценка стоимости';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+const screenPrice = 5000;
+const percentage = 5;
+const fullPrice = 50000;
+const responsive = 25;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice, 'цена реализации одного экрана');
+console.log(percentage, 'процент отката подрядчику');
+console.log(fullPrice, 'желаемая сумма зароботка с проекта');
+console.log(screenPrice * responsive, 'стоимость за', responsive, 'экранов');
