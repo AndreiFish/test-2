6eb840dd2477b7d3111074722c5c03c138d1b495
AndreiFish <and.zhandrov@gmail.com>
2025-02-26 12:55:13 +0400

    first commit

--- homework 4/homework 4.js
+++ homework 4/homework 4.js
@@ -3,11 +3,11 @@ const screensValue = 'шаблонные, с уникальным дизайно
 const screenPrice = 5000;
 const percentage = 5;
 const fullPrice = 50000;
-const responsive = 25;
+const responsive = true;
 
 console.log(titleProject);
 console.log(screensValue);
 console.log(screenPrice, 'цена реализации одного экрана');
 console.log(percentage, 'процент отката подрядчику');
 console.log(fullPrice, 'желаемая сумма зароботка с проекта');
-console.log(screenPrice * responsive, 'стоимость за', responsive, 'экранов');
+console.log(responsive);
